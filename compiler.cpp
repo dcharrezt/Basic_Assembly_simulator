a46e91253549329d59e91ba379696b8e9d5659be
Diego Charrez <dcharrezt@unsa.edu.pe>
2018-06-15 23:51:51 -0500

    undetected bug

--- compiler.cpp
+++ compiler.cpp
@@ -2,8 +2,9 @@
 
 void Compiler_asm::compile( std::string file ) {
 	this->fileName = file;
-
+	int accumulator;
 	init_functionMap();
+
 
 	std::string line;
 	std::ifstream program( fileName );
@@ -11,98 +12,144 @@ void Compiler_asm::compile( std::string file ) {
 		getline(program, line);
 		physicMemory.push_back( line );
 	}
-	for (int i = 0; i < physicMemory.size(); ++i)
+
+	for (int programCounter = 0; programCounter < physicMemory.size(); ++programCounter)
 	{
-		process( physicMemory[i] );
+		process( physicMemory[programCounter] );
 	}
 }
 
-int Compiler_asm::process( std::string line ) {
-
-	std::cout << line << " "<< line[0] << std::endl;
-	if( line[0] != 0)
-		functionMap[line.substr(1, 3)];
-	else {
-		functionMap[line];
-	}
+int Compiler_asm::process( std::string line) {
+	std::string index;
+	int value;
+	std::cout << " Code " << line << " "<< line[0] << std::endl;
+	if( line[0] != '0') {
+		index = line[0]; 
+		value = std::stoi( line.substr(1, 3), 0, 16 );
+		call_script( index, value );
+	}	
+	else
+		call_script( line, value );
 
 	return 0;
 }
 
-void Compiler_asm::init_functionMap( ){
-	functionMap.emplace("0000",);
-	functionMap.emplace("0001",);
-	functionMap.emplace("1",);
-	functionMap.emplace("2",);
-	functionMap.emplace("3",);
-	functionMap.emplace("4",);
-	functionMap.emplace("5",);
-	functionMap.emplace("6",);
-	functionMap.emplace("7",);
-	functionMap.emplace("8",);
-	functionMap.emplace("9",);
-	functionMap.emplace("A",);
+void Compiler_asm::call_script(const std::string& pFunction, int value)
+{
+    auto iter = functionMap.find(pFunction);
+    if (iter == functionMap.end())
+    {
+        std::cout << "Function not found " << std::endl;
+    }
+
+    (*iter->second)(value);
 }
 
 
+void Compiler_asm::init_functionMap() {
+	functionMap.emplace("0000", &HALT);
+	functionMap.emplace("0001", &NEGATE);
+	functionMap.emplace("1", &MLOAD);
+	functionMap.emplace("2", &DLOAD);
+	functionMap.emplace("3", &ILOAD);
+	functionMap.emplace("4", &DSTORE);
+	functionMap.emplace("5", &ISTORE);
+	functionMap.emplace("6", &JMP);
+	functionMap.emplace("7", &JZ);
+	functionMap.emplace("8", &JP);
+	functionMap.emplace("9", &JN);
+	functionMap.emplace("A", &ADD);
+	// functionMap["0000"] = HALT;
+	// functionMap["0001"]= NEGATE;
+	// functionMap["1"] = MLOAD;
+	// functionMap["2"] = DLOAD;
+	// functionMap["3"] = ILOAD;
+	// functionMap["4"] = DSTORE;
+	// functionMap["5"] = ISTORE;
+	// functionMap["6"] = JMP;
+	// functionMap["7"] = JZ;
+	// functionMap["8"] = JP;
+	// functionMap["9"] = JN;
+	// functionMap["A"] = ADD;
+}
 
 void MLOAD( int value ) {
-	int acc = std::stoi( value, 0, 10 )
+	int accumulator = value;
+	std::cout << "accumulator = " << accumulator << std::endl;
 }
 
 void DLOAD( int value ) {
-	acc = mem[value];
+	int memory[128];
+	int accumulator = memory[ value ];
+	std::cout << "accumulator = " << accumulator << std::endl;
 }
 
 void ILOAD( int value ) {
-	acc = mem[ mem[value] ];
+	int memory[128];
+	// std::cout << "M = " << value << std::endl;
+	std::cout << "accumulator = " << memory[ value ] << std::endl;
+
+	int accumulator = memory[memory[ value ]];
 }
 
 void DSTORE( int value ) {
-	int acc;
-	std::unordered_map<std::string, std::string> memory;
-	acc += std::stoi( memory[value], 0, 16 );
+	int accumulator;
+	int memory[128];
+	memory[ value ] = accumulator; 
 }
 
 void ISTORE( int value ) {
-	int acc;
-	int mem[50];
-	mem[ mem[value] ] = acc;
+	int accumulator;
+	int memory[128];
+	memory[ memory[value] ] = accumulator;
 }
 
-void HALT( int value ) {
+void HALT( int val) {
+	std::cout << "Program Ended " << std::endl;
 	return;
 }
 
 void JMP( int dir ) {
-	pc = dir;
+	int accumulator;
+	int programCounter;
+	programCounter = dir;
 }
 
 void JZ( int value )  {
-	if( acc = 0 );
+	int accumulator;
+	int programCounter;
+	if( accumulator = 0 )
+		programCounter = value;
 
 }
 
 void JP( int value ) {
-	if( acc > 0 );
-		pc = value;
+	int accumulator;
+	int programCounter;
+	if( accumulator > 0 )
+		programCounter = value; 
 	else
-		pc++;
+		programCounter++;
 }
 
 void JN( int value ) {
-	if( acc < 0 )
-		pc = value;
+	int accumulator;
+	int programCounter;
+	if( accumulator < 0 )
+		programCounter = value;
 	else
-		pc++;
+		programCounter++;
 }
 
 void ADD( int value ) {
-	std::unordered_map<std::string, std::string> memory;
-	std::string acc;
-	memory[value];
+	int accumulator;
+	int memory[128];
+	accumulator += memory[value];
 }
 
-void NEGATE( int value ) {
-	acc = -1*acc;
+void NEGATE( int val) {
+	std::cout << val << std::endl;
+	int accumulator;
+	accumulator = -1*accumulator;
+	std::cout<< "aacc " << accumulator << std::endl;
 }
--- compiler.h
+++ compiler.h
@@ -5,33 +5,35 @@
 typedef void (*ScriptFunction)( int );
 typedef std::unordered_map<std::string, ScriptFunction> script_map;
 
-extern mem[128];
-extern int acc;
-extern int pc;
-extern std::unordered_map<std::string, std::string> memory;
+extern int memory[128];
+
+extern int accumulator;
+extern int programCounter;
 
 void MLOAD( int value ); 
 void DLOAD( int value ); 
 void ILOAD( int value ); 
 void DSTORE( int value ); 
 void ISTORE( int value ); 
-void HALT( int value ); 
+void HALT( int val); 
 void JMP( int value ); 
 void JZ( int value ); 
 void JP( int value ); 
 void JN( int value ); 
 void ADD( int value ); 
-void NEGATE( int value ); 
+void NEGATE( int val ); 
 
 class Compiler_asm {
 public:
-	int fileName;
-	std::vector< int > physicMemory;
+	std::string fileName;
+	std::vector< std::string > physicMemory;
 	script_map functionMap;
+	int memory[128] = {0};
 
 public:
-	void compile( int file );
-	int process( int line );
+	void compile( std::string file );
+	int process( std::string line );
+	void call_script(const std::string& pFunction, int value);
 
 	void init_functionMap();
 };
